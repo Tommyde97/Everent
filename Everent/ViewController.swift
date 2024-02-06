2fb83b0317c1ec633e3387f5383706e4d1b18690
Tommy <tommyde97@hotmail.com>
2024-02-06 14:10:41 -0500

    Initial Commit

---
+++ Everent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Everent
+//
+//  Created by Tomas D. De Andrade Gomes on 2/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
